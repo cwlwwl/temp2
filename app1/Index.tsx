d0fd05373895fb2772a3ac1a01a67fe4dbb78550
wgr <cwlwwl@163.com>
2021-04-13 16:43:17 +0800

    first

---
+++ app1/Index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Index() {
+	return (
+		<ul>
+			<li>格物</li>
+			<li>知智</li>
+			<li>意得</li>
+			<li>心正</li>
+			<li>修身</li>
+			<li>齐家</li>
+			<li>治国</li>
+			<li>平天下</li>
+		</ul>
+	);
+}
+
+export default Index;
---
+++ app1/Teacher.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Teahcer() {
+  return (
+    <p>这是老师</p>
+  );
+}
+
+export default Teahcer;
